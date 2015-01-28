2004187165b03e6774358edb2e1a329e20db748a
Rizwan Ali <rizcube@gmail.com>
2015-01-28 16:29:57 +0000

    attempt to translate one to ten

---
+++ Beer_Bottle_song.rb
@@ -0,0 +1,69 @@
+# Write a program that outputs the lyrics for "Ninety-nine Bottles of Beer on the Wall"
+# Your program should print the number of bottles in English, not as a number. For example:
+#
+# Ninety-nine bottles of beer on the wall,
+# Ninety-nine bottles of beer,
+# Take one down, pass it around,
+# Ninety-eight bottles of beer on the wall.
+# ...
+# One bottle of beer on the wall,
+# One bottle of beer,
+# Take one down, pass it around,
+# Zero bottles of beer on the wall.
+#
+# Your program should not use ninety-nine output statements!
+# Design your program with a class named BeerSong whose initialize method
+# receives a parameter indicating the number of bottles of beer initially on the wall.
+# If the parameter is less than zero, set the number of bottles to zero. Similarly,
+# if the parameter is greater than 99, set the number of beer bottles to 99
+# Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
+# Add any additional methods you find helpful.
+
+class BeerSong
+ 
+    attr_accessor :beers_qty
+
+	def initialize(beers_qty)
+		@beers_qty = beers_qty
+		@beers_qty = 0 if beers_qty < 0
+		@beers_qty = 99 if beers_qty > 99
+
+        print_song(beers_qty)
+        translate(beers_qty)
+	end
+
+	def print_song(beers_qty)
+		beers_qty.downto 1 do |i|
+			puts i
+		end
+	end
+
+    def translate(beers_qty)
+            
+            oneToten = {
+            1 => "one",
+            2 => "two",
+            3 => "three",
+            4 => "four",
+            5 => "five",
+            6 => "six",
+            7 => "seven",
+            8 => "eight",
+            9 => "nine",
+            10 => "ten"
+        }
+            
+            
+            oneToten.each do |beers_qty , translate|
+            puts "#{beers_qty}: #{translate}"
+            end    
+    end
+
+
+
+
+end
+
+beers = BeerSong.new(5)
+#puts beers.beers_qty
+
